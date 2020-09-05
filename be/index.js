d26af5015d9de63e284ae5248f06160f7693663f
Yasin <yasinvural0@gmail.com>
2020-09-05 21:07:37 +0300

    cors package is added

--- be/index.js
+++ be/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = 4000;
 
+app.use(express.json());
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "*");
+//   next();
+// });
+
 app.get("/", (req, res) => {
-  res.send("Hello  Node.js!");
+  res.status(200).send("Hello Node.js !!");
 });
 
 app.listen(port, () => {
